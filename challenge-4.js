a858e00a50f673e161174d9206d58cc288f945ab
Akash Sahani <akash.sahani@ambab.com>
2021-09-20 10:39:28 +0530

    Some modifications done

--- challenge-4.js
+++ challenge-4.js
@@ -8,7 +8,7 @@ class Person {
         this.mobileNumber = mobileNumber;
     }
 
-    fullName = (firstName, lastName) => console.log(firstName + " " + lastName);
+    fullName = (firstName, lastName) => `${firstName} ${lastName}`;
 }
 
 createPerson = (firstName, lastName, mobileNumber) => {
@@ -23,8 +23,8 @@ createPerson = (firstName, lastName, mobileNumber) => {
                 else {
                     //if Mobile number not exist in users array it will push data to array
                     const obj = new Person(firstName, lastName, mobileNumber)
-                    let PersonData = {firstName: obj.firstName, lastName: obj.lastName, mobileNumber: obj.mobileNumber};
-                    users.push(PersonData);
+                    // let PersonData = {firstName: obj.firstName, lastName: obj.lastName, mobileNumber: obj.mobileNumber};
+                    users.push(obj);
                     resolve(obj.fullName(firstName, lastName));
                 }
             },
@@ -37,32 +37,44 @@ const main = () => {
     //Promise chaining
 
     createPerson("Bhavesh", "Sahani", 123456789) //1st
-    .then(() => createPerson("Akash", "Sahani", 111111)) //2nd
+    .then((result) => console.log(result)) 
     .catch((error) => console.log(error))
 
-    .then(() => createPerson("Rishabh", "Tiwari", 123456789)) //3rd
+    createPerson("Akash", "Sahani", 111111) //2nd
+    .then((result) => console.log(result))
     .catch((error) => console.log(error))
 
-    .then(() => createPerson("Lovelesh", "Gupta", 78787878)) //4th
+    createPerson("Rishabh", "Tiwari", 123456789) //3rd
+    .then((result) => console.log(result))
     .catch((error) => console.log(error))
 
-    .then(() => createPerson("Vikas", "Gupta", 987654312)) //5th
+    createPerson("Lovelesh", "Gupta", 78787878) //4th
+    .then((result) => console.log(result))
     .catch((error) => console.log(error))
 
-    .then(() => createPerson("Bhavesh", "Yadav", 23456461)) //6th
+    createPerson("Vikas", "Gupta", 987654312) //5th
+    .then((result) => console.log(result))
     .catch((error) => console.log(error))
 
-    .then(() => createPerson("Siddhesh", "More", 987654312)) //7th
+    createPerson("Bhavesh", "Yadav", 23456461) //6th
+    .then((result) => console.log(result))
     .catch((error) => console.log(error))
 
-    .then(() => createPerson("John", "Doe", 123456789)) //8th
+    createPerson("Siddhesh", "More", 987654312) //7th
+    .then((result) => console.log(result))
     .catch((error) => console.log(error))
 
-    .then(() => createPerson("Arvind", "Kumar", 123321123)) //9th
+    createPerson("John", "Doe", 123456789) //8th
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+
+    createPerson("Arvind", "Kumar", 123321123) //9th
+    .then((result) => console.log(result))
     .catch((error) => console.log(error))
     
-    .then(() => createPerson("Unknown", "Person", 123321123)) //10th
-    .catch((error) => console.log(error));
+    createPerson("Unknown", "Person", 123321123) //10th
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
 }
 
 main();
